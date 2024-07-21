d240e87d873b7c82f920653bc1c8004f41d8bfe8
leohernandes <leohernandesvasc@gmail.com>
2024-07-21 20:13:26 -0300

    add new tests

--- scripts/comparation.sh
+++ scripts/comparation.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
+ERR_UNDECLARED=10
+ERR_DECLARED=11
+ERR_VARIABLE=20
+ERR_FUNCTION=21
+
 make
-for file in $(ls tests --hide="*.dot"); do
-    ./etapa3 < tests/$file | ./scripts/output2dot.sh > tests/$file.dot;
+for file in $(ls tests); do
+    echo $file
+    answer=$(grep '//' tests/$file)
+    grep_code=$?
+    answer=$(echo $answer | tr -d '/') 
+    if [ $grep_code -eq 0 ]; then
+        ./etapa4 < tests/$file;
+        code=$?
+        if [ $code -eq $ERR_UNDECLARED ] && [ "$answer" = "ERR_UNDECLARED" ]; then
+            echo "✅ Correct!"
+        elif [ $code -eq $ERR_DECLARED ] && [ "$answer" = "ERR_DECLARED" ]; then
+            echo "✅ Correct!"
+        elif [ $code -eq $ERR_VARIABLE ] && [ "$answer" = "ERR_VARIABLE" ]; then
+            echo "✅ Correct!"
+        elif [ $code -eq $ERR_FUNCTION ] && [ "$answer" = "ERR_FUNCTION" ]; then
+            echo "✅ Correct!"
+        else
+            echo "❌ Wrong!: $code" 
+        fi
+    else
+        ./etapa4 < tests/$file
+        code=$?
+        if [ $code -eq 0 ]; then
+            echo "✅ Correct!"
+        else
+            echo "❌ Wrong!"
+        fi
+    fi
 done
 
-for i in {00..59}; do
-    dot_file="tests/z${i}.dot"
-    ref_dot_file="tests/z${i}.ref.dot"
 
-   
-    xdot "$dot_file" &
-    xdot  "$ref_dot_file" 
-   
-    while pgrep -x "xdot" >/dev/null; do
-        sleep 1
-    done
-done
