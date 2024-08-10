1180fa7883cf4eccdecbe396596daf4da641c2ad
leohernandes <leohernandesvasc@gmail.com>
2024-08-10 12:19:23 -0300

    initial printing of code segment

--- include/asm.h
+++ include/asm.h
@@ -5,11 +5,6 @@
 #include <iloc.h>
 #include <symbolTable.h>
 
-void print_main_function_info();
-void print_global_variable_info(char *identifier, int variable_pos);
-void print_global_variables_info(iloc_program_t *program, symbol_table_t *global_table);
-void print_data_segment(iloc_program_t *program, symbol_table_t *global_table);
-void print_code_segment(iloc_program_t *program, symbol_table_t *global_table);
 void print_asm(iloc_program_t *program, symbol_table_t *global_table);
 
 #endif
--- src/asm.c
+++ src/asm.c
@@ -1,32 +1,35 @@
 #include "asm.h"
 
-void print_main_function_info()
+/* ================================== Data Segment ================================== */
+
+void _print_main_function_info()
 {
     printf(
-        "\t.long\t3\n"
         "\t.text\n"
         "\t.globl\tmain\n"
         "\t.type\tmain, @function\n"
         "main:\n");
 }
-void print_global_variable_info(char *identifier, int variable_pos)
+void _print_global_variable_info(char *identifier, int variable_pos)
 {
     printf("\t.globl\t%s\n", identifier);
 
     if (variable_pos == 0)
     {
-        printf("\t.data\n");
+        printf("\t.bss\n"); // <== Block Starting Symbol: portion of an asm code that contains statically
+                            //     allocated variables that have not been assigned a value yet
     }
 
     printf(
         "\t.align 4\n"
         "\t.type\t%s, @object\n"
         "\t.size\t%s, 4\n"
-        "%s:\n",
+        "%s:\n"
+        "\t.zero\t4\n", // <== Initialize the 4 byte space with zeros for the variable
         identifier, identifier, identifier);
 }
 
-void print_global_variables_info(iloc_program_t *program, symbol_table_t *global_table)
+void _print_global_variables_info(symbol_table_t *global_table)
 {
     symbol_t *current_symbol;
     int variable_pos = 0, table_pos = 0;
@@ -38,7 +41,7 @@ void print_global_variables_info(iloc_program_t *program, symbol_table_t *global
         {
             if (current_symbol->nature == IDENTIFIER_NATURE)
             {
-                print_global_variable_info(current_symbol->lex_data->lexeme, variable_pos);
+                _print_global_variable_info(current_symbol->lex_data->lexeme, variable_pos);
             }
             variable_pos++;
         }
@@ -46,44 +49,109 @@ void print_global_variables_info(iloc_program_t *program, symbol_table_t *global
     }
 }
 
-void print_data_segment(iloc_program_t *program, symbol_table_t *global_table)
+void _print_data_segment(iloc_program_t *program, symbol_table_t *global_table)
 {
-    print_global_variables_info(program, global_table);
-    print_main_function_info();
+    _print_global_variables_info(global_table);
+    _print_main_function_info();
 }
 
-void print_asm(iloc_program_t *program, symbol_table_t *global_table)
+/* ================================== Code Segment ================================== */
+
+void _print_instruction(iloc_instruction_t instruction, symbol_table_t *global_table)
 {
-    print_data_segment(program, global_table);
-    print_code_segment(program, global_table);
+    switch (instruction.op_code)
+    {
+    case ADD:
+        break;
+
+    case AND:
+        break;
+
+    case CBR:
+        break;
+
+    case CMP_EQ:
+        break;
+
+    case CMP_GE:
+        break;
+
+    case CMP_GT:
+        break;
+
+    case CMP_LE:
+        break;
+
+    case CMP_LT:
+        break;
+
+    case CMP_NE:
+        break;
+
+    case DIV:
+        break;
+
+    case JUMPI:
+        break;
+
+    case LOADAI:
+        char *scope = instruction.operand_1;
+        char *offset = instruction.operand_2;
+        if (strcmp(scope, "rbss") == 0)
+        {
+            char *label = symbol_table_get_identifier_label_from_offset_or_null(global_table, (unsigned int)atoi(offset));
+            printf("movl\t%s(\%rip), \%edx", label);
+        }
+        else
+        {
+            printf("movl\t-%s(%rbp), %eax", offset);
+        }
+        break;
+
+    case LOADI:
+        break;
+
+    case MULT:
+        break;
+
+    case MULTI:
+        break;
+
+    case NOP:
+        break;
+
+    case OR:
+        break;
+
+    case RET:
+        break;
+
+    case STOREAI:
+        break;
+
+    case SUB:
+        break;
+    }
 }
 
-void print_code_segment(iloc_program_t *program, symbol_table_t *global_table)
+void _print_code_segment(iloc_program_t *program, symbol_table_t *global_table)
 {
-    iloc_instruction_t instruction;
+    iloc_instruction_t *instruction = program->instructions;
     char *label, *scope, *offset;
 
     for (int pos = 0; pos < program->length; pos++)
     {
-        instruction = program->instructions[pos];
-        if (instruction.op_code == LOADAI ||
-            instruction.op_code == STOREAI)
-        {
-            if (instruction.op_code == LOADAI)
-            {
-                scope = instruction.operand_1;
-                offset = instruction.operand_2;
-            }
-            else
-            {
-                scope = instruction.operand_2;
-                offset = instruction.operand_3;
-            }
-            label = symbol_table_get_identifier_label_from_offset_or_null(global_table, (unsigned int)atoi(offset));
-            if (label != NULL)
-            {
-                printf("Global variable %s used in %s instruction with %s offset\n", label, instruction.op_code, offset);
-            }
-        }
+        _print_instruction(program->instructions[pos], global_table);
     }
+}
+
+/* ================================== Main Printing Function ================================== */
+
+void print_asm(iloc_program_t *program, symbol_table_t *global_table)
+{
+    printf("\t.file\t\"etapa6.c\"\n");
+    printf("\t.text\n");
+
+    _print_data_segment(program, global_table);
+    _print_code_segment(program, global_table);
 }
